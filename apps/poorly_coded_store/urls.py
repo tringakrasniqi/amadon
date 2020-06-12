240ac45cb7a0fb68ea63940aa2409dbbe87ed459
tringak <tringaakrasniqi@gmail.com>
2020-06-12 22:27:16 +0200

    Fixed form issues: rendering/redirecting

--- apps/poorly_coded_store/urls.py
+++ apps/poorly_coded_store/urls.py
@@ -1,7 +1,8 @@
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^checkout$', views.checkout)
+    path('', views.index),
+    path('order', views.order),
+    path('checkout/<int:order_id>', views.checkout)
 ]
--- apps/poorly_coded_store/views.py
+++ apps/poorly_coded_store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Order, Product
 
 def index(request):
@@ -7,10 +7,28 @@ def index(request):
     }
     return render(request, "store/index.html", context)
 
-def checkout(request):
+def order(request):
+    price_from_form = Product.objects.get(id = request.POST['product_id']).price
     quantity_from_form = int(request.POST["quantity"])
-    price_from_form = float(request.POST["price"])
     total_charge = quantity_from_form * price_from_form
-    print("Charging credit card...")
-    Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
-    return render(request, "store/checkout.html")
+    order = Order.objects.create(quantity_ordered=quantity_from_form, total_price=total_charge)
+    return redirect(f'/checkout/{order.id}')
+
+def checkout(request, order_id):
+    order = Order.objects.get(id=order_id)
+    all_orders = Order.objects.all()
+
+    all_orders_price = 0
+    all_orders_total_quantity = 0
+
+    for order in all_orders:
+        all_orders_price += order.total_price
+        all_orders_total_quantity += order.quantity_ordered
+
+    context = {
+        'quantity' :  order.quantity_ordered,
+        'charged' :  order.total_price,
+        'all_orders_price' : all_orders_price,
+        'all_orders_total_quantity' : all_orders_total_quantity
+    }
+    return render(request, "store/checkout.html", context)
